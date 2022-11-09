312c4315e1d78017a21ca307e95e536f9d895186
Touch Grass <toberock101@gmail.com>
2022-11-08 22:22:17 -0800

    Changed layout of app.

--- partial/js/main.js
+++ partial/js/main.js
@@ -1,20 +1,28 @@
 "use strict";
 const docs = {
     id: window.location.href.split('/document/d/')[1].split('/')[0],
+    /**
+     * @returns {string} - The document title
+     */
     name() {
-        var _a;
-        return (_a = document.querySelector('.docs-title-input-label-inner').textContent) === null || _a === void 0 ? void 0 : _a.trim();
+        var _a, _b;
+        return ((_b = (_a = document.querySelector('.docs-title-input-label-inner').textContent) === null || _a === void 0 ? void 0 : _a.trim()) !== null && _b !== void 0 ? _b : '');
     },
-};
-docs.pasteText = function (text) {
-    const el = document.querySelectorAll('docs-texteventtarget-iframe')[0].contentDocument.querySelector('[contenteditable=true]');
-    const data = new DataTransfer();
-    data.setData('text/plain', text);
-    const paste = new ClipboardEvent('paste', {
-        clipboardData: data,
-        bubbles: true,
-        cancelable: true,
-    });
-    el.dispatchEvent(paste);
+    /**
+     * Pastes the given text into the document.
+     * @param {string} text - Text that will be pasted into the document.
+     */
+    pasteText(text) {
+        /** Element to paste text into */
+        const el = document.querySelectorAll('docs-texteventtarget-iframe')[0].contentDocument.querySelector('[contenteditable=true]');
+        const data = new DataTransfer();
+        data.setData('text/plain', text);
+        const paste = new ClipboardEvent('paste', {
+            clipboardData: data,
+            bubbles: true,
+            cancelable: true,
+        });
+        el.dispatchEvent(paste);
+    },
 };
 console.log(docs.name());
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,28 +1,38 @@
 const docs: Record<string, any> = {
   id: window.location.href.split('/document/d/')[1].split('/')[0],
 
-  name() {
+  /**
+   * @returns {string} - The document title
+   */
+  name(): string {
     return (
-      document.querySelector('.docs-title-input-label-inner') as HTMLElement
-    ).textContent?.trim();
+      (
+        document.querySelector('.docs-title-input-label-inner') as HTMLElement
+      ).textContent?.trim() ?? ''
+    );
   },
-};
 
-docs.pasteText = function (text: string): void {
-  const el = (
-    (
-      document.querySelectorAll(
-        'docs-texteventtarget-iframe'
-      )[0] as HTMLIFrameElement
-    ).contentDocument as Document
-  ).querySelector('[contenteditable=true]') as HTMLElement;
-  const data = new DataTransfer();
-  data.setData('text/plain', text);
-  const paste = new ClipboardEvent('paste', {
-    clipboardData: data,
-    bubbles: true,
-    cancelable: true,
-  });
-  el.dispatchEvent(paste);
+  /**
+   * Pastes the given text into the document.
+   * @param {string} text - Text that will be pasted into the document.
+   */
+  pasteText(text: string): void {
+    /** Element to paste text into */
+    const el = (
+      (
+        document.querySelectorAll(
+          'docs-texteventtarget-iframe'
+        )[0] as HTMLIFrameElement
+      ).contentDocument as Document
+    ).querySelector('[contenteditable=true]') as HTMLElement;
+    const data = new DataTransfer();
+    data.setData('text/plain', text);
+    const paste = new ClipboardEvent('paste', {
+      clipboardData: data,
+      bubbles: true,
+      cancelable: true,
+    });
+    el.dispatchEvent(paste);
+  },
 };
 console.log(docs.name());
